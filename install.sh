13d013a713cc938f0b1419a7bdcd8e379fbe40f0
Felix Serang <byte007@zoho.com>
2023-06-07 11:29:56 +0700

    feat: display help

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+###################################
+# Help
+###################################
+Help() {
+	# Display help
+	echo ""
+  	echo "Usage: ./install.sh [-h|i|V]"
+	echo ""
+	echo "Option description:"
+	echo "-------------------"
+	echo "h	Print this help."
+	echo "i	Install software."
+	echo "V	Print software version and exit."
+	echo ""
+}
+
+
+### Main program
+while getopts ":h" option; do
+    case $option in
+        h) 
+            Help
+            exit;;
+    esac
+done
